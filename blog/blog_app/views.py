ce9a7a6b5b060bc3294ade9051483077fd92f038
FeliksNovoselskyi <felixdnbox@gmail.com>
2026-09-22 21:13:40 +0300

    Add jinja expressions (for, if and lower)

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def render_index(request):
-    
+
+    name = "arseniy"
+
+    list_products = ["banana", "persimmon", "kiwi"]
+
     return render(
         request=request,
-        template_name="blog_app/index.html"
+        template_name="blog_app/index.html",
+        # несколько значений
+
+        # контекст шаблона
+        # значения
+
+        # context -
+        # контекст в формате словаря
+        context={
+            "name": name,
+            "list_products": list_products
+        }
     )
-    
+
